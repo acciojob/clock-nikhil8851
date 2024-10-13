9b1dc4ad765cd9a4fabc0c644cbb0f07b25a2460
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-10-12 23:05:15 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function updatetime() {
+	 const time = document.querySelector(`#timer`)
+	const now = new Date()
+	const str = now.toLocaleString(`en-IN`,{
+		month: `2-digit`,
+		day: `2-digit`,
+		year: `numeric`,
+		hour: `2-digit`,
+		minute: `2-digit`,
+		second: `2-digit`,
+		hour12:true
+	})
+	time.textContent = str;
+}
+setInterval(updatetime,1000);
+	 updatetime();
